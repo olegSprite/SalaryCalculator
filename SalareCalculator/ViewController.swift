72d32d343c29945d5f562d6e5b4de961b96406b6
Oleg Spiridonov <spiri300@yandex.ru>
2024-01-19 01:26:18 +0500

    Initial Commit

---
+++ SalareCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SalareCalculator
+//
+//  Created by Олег Спиридонов on 19.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
