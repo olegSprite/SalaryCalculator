12f96d509ee974355ba8a10a06d36c39f8dcd7f6
Oleg Spiridonov <spiri300@yandex.ru>
2024-01-19 20:27:13 +0500

    Предварительно сверстал экран для создания новой зп, нужно расставить констрейнты. Добавил функции для пикера

--- SalareCalculator/CounterViewController.swift
+++ SalareCalculator/CounterViewController.swift
@@ -10,13 +10,31 @@ import UIKit
 
 class CounterViewController: UIViewController {
     
-    @IBOutlet weak var mounthPicker: UIPickerView!
+    @IBOutlet weak private var mounthPicker: UIPickerView!
+ 
+    private var curentMounth: Mounth?
+    private let mounth: [Mounth] = [
+        .january,
+        .february,
+        .march,
+        .april,
+        .may,
+        .june,
+        .july,
+        .august,
+        .september,
+        .october,
+        .november,
+        .december
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 }
+
+// MARK: - PickerViewDataSource
 
 extension CounterViewController: UIPickerViewDataSource {
     
@@ -25,30 +43,45 @@ extension CounterViewController: UIPickerViewDataSource {
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 12
+        return mounth.count
     }
 }
 
+// MARK: - PickerViewDelegate
+
 extension CounterViewController: UIPickerViewDelegate {
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        let mounth: [Mounth] = [
-            .january,
-            .february,
-            .march,
-            .april,
-            .may,
-            .june,
-            .july,
-            .august,
-            .september,
-            .october,
-            .november,
-            .december
-        ]
-        
-        for result in mounth {
-            return result.rawValue
+        return mounth[row].rawValue
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch row {
+        case 0:
+            curentMounth = .january
+        case 1:
+            curentMounth = .february
+        case 2:
+            curentMounth = .march
+        case 3:
+            curentMounth = .april
+        case 4:
+            curentMounth = .may
+        case 5:
+            curentMounth = .june
+        case 6:
+            curentMounth = .july
+        case 7:
+            curentMounth = .august
+        case 8:
+            curentMounth = .september
+        case 9:
+            curentMounth = .october
+        case 10:
+            curentMounth = .november
+        case 11:
+            curentMounth = .december
+        default:
+            return
         }
-        return nil
     }
 }
