2850bd4e3e4fd0e91eedd337634cd89fb4679781
Oleg Spiridonov <spiri300@yandex.ru>
2024-01-19 14:10:30 +0500

    Переверстал первый экран, добавил оутлеты и модель зарплаты. Начинаю верстать экан подсчета зарплаты, остановился на пикере месяца

--- SalareCalculator/CounterViewController.swift
+++ SalareCalculator/CounterViewController.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import UIKit
+
+class CounterViewController: UIViewController {
+    
+    @IBOutlet weak var mounthPicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+extension CounterViewController: UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 12
+    }
+}
+
+extension CounterViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return nil // Нужно как то возвращать ассоциативные значения из перечисления месяцев
+    }
+}
--- SalareCalculator/SalaryModel.swift
+++ SalareCalculator/SalaryModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+struct SalaryModel {
+    var hoursMounth: Int
+    var hoursHalf: Int
+    var nightHourseMounth: Int
+    var nightHourseHalf: Int
+    var selebrationHourse: Int
+    var allSalary: Int
+    var firstHalfSalary: Int
+    var secondHalfSalary: Int
+    var mounth: Mounth
+}
+
+enum Mounth: String {
+    case january = "Январь"
+    case february = "Февраль"
+    case march = "Март"
+    case april = "Апрель"
+    case may = "Май"
+    case june = "Июнь"
+    case july = "Июль"
+    case august = "Август"
+    case september = "Сентябрь"
+    case october = "Октябрь"
+    case november = "Ноябрь"
+    case december = "Декабрь"
+}
